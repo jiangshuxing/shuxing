ac7577801ce829fa429dd4ad32fe44df4103d746
birdPeople <543796338@qq.com>
2022-09-28 23:24:44 +0800

    add my framework

---
+++ shuxing-framework/shuxing-common/src/main/java/com/shuxing/framework/util/object/PageUtils.java
@@ -0,0 +1,17 @@
+package com.shuxing.framework.util.object;
+
+
+import com.shuxing.framework.pojo.PageParam;
+
+/**
+ * {@link PageParam} 工具类
+ *
+ * @author 芋道源码
+ */
+public class PageUtils {
+
+    public static int getStart(PageParam pageParam) {
+        return (pageParam.getPageNo() - 1) * pageParam.getPageSize();
+    }
+
+}
